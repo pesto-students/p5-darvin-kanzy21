d0815651f758337ebd2be5f4382fe03ca867bca4
shreyas-k <shreyaskanzarkar@gmail.com>
2022-09-23 13:52:33 +0530

    tasks with async await and generators

---
+++ Week-5/solution1/asyncAwait.js
@@ -0,0 +1,38 @@
+async function doTask1() {
+    return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(`Slept for: 2s`);
+		}, "2000")
+	})
+}
+async function doTask2() {
+    return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(`Slept for: 3s`)
+
+		}, "3000")
+	})
+}
+async function doTask3() {
+    return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(`Slept for: 1s`)
+		}, "1000")
+	})
+}
+async function main() {
+	// 1. 
+	console.time('main')
+
+	// 2.
+	console.log(await doTask1())
+	console.log(await doTask2(2))
+	console.log(await doTask3(3).then((res)=>{
+        console.log(res)
+    }))
+
+	// 3.
+	console.timeEnd('main')
+}
+
+main()
